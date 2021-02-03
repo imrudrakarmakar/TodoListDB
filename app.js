51fc3947fe53fbc935c29a0092eaa99fa4367006
Rudra Karmakar <47749203+imrudrakarmakar@users.noreply.github.com>
2021-02-03 21:04:23 +0530

    Modified Local MongoDB

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-rudra:Test123@cluster0.xn19e.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
